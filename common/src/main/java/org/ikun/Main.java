16560d7003aba0bd305fb8e08ef8920e3f794ece
wljQAQ <wulongjiang1@gmail.com>
2023-10-30 22:06:05 +0800

    环境配置

--- common/src/main/java/org/ikun/Main.java
+++ common/src/main/java/org/ikun/Main.java
@@ -1,7 +1,13 @@
 package org.ikun;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//主程序
+@SpringBootApplication
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
+
 }
